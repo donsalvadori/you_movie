efc8176674343f843f614cb7ac30bb2dd9da070c
donsalvadori <csalvi42@gmail.com>
2015-05-05 17:02:55 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Youmovie::Application.config.secret_key_base = 'ddfda698aca0998835038db0e5fecaad7d939432dc147d5ccc12f021faf18dd0542f4aedd4a082f290263f91f63a4d4071fe5bc75f7823fbc1fab668bfc27673'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Youmovie::Application.config.session_store :cookie_store, key: '_youmovie_session'
